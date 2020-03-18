589cf75832a1b1cd7665b9c2e28df7f21629c7c8
Karim Macknojia <alishahal@msn.com>
2020-03-18 05:22:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand, :color, :size, :condition, : material
+  attr_accessor :brand, :color, :size, :condition, :material
   # attr_reader :material
   
   def initialize(brand)
